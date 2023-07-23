2016c9b6b2c2873caf37cfba49fa7fc588777f79
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-07-23 14:16:24 -0300

    Update App.js

--- App.js
+++ App.js
@@ -9,29 +9,6 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            if (route.name === 'Feed') {
-              iconName = focused
-                ? 'book'
-                : 'book-outline';
-            } else if (route.name === 'Criar História') {
-              iconName = focused ? 'create' : 'create-outline';
-            }
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
-        }}
-      >
-        <Tab.Screen name="Feed" component={Feed} />
-        <Tab.Screen name="Criar História" component={CreateStory} />
-      </Tab.Navigator>
-    </NavigationContainer>
+
   );
 }
